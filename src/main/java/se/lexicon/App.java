8f730f1d5d733b2a51f9938472c1ae6477d6f6ff
NiveSJ <niveathithavj@gmail.com>
2022-12-28 12:17:24 +0100

    Added annotation

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,5 +1,6 @@
 package se.lexicon;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,9 +12,10 @@ public class App
     public static void main( String[] args )
     {
 
-        ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context= new
+                AnnotationConfigApplicationContext(SportConfig.class);
         Coach coach= context.getBean("MyCoach", Coach.class);
-        System.out.println(   coach.getDailyWorkout());
+        System.out.println( "Spring IOC Concept"+  coach.getDailyWorkout());
         context.close();
 
 
